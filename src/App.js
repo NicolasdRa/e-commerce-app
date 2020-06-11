ac457f1093941cbde742700831e3c846ba72cce3
Nicolas <nicolasdirago@gmail.com>
2020-06-11 17:32:46 +0200

    added header component & upper menu links - previous commit changed to NPM package manager

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Header from './components/ui/header/Header'
 import Home from './components/pages/home/Home'
 import Shop from './components/pages/shop/Shop'
 import './App.css'
@@ -7,6 +8,7 @@ import './App.css'
 function App () {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/shop' component={Shop} />
