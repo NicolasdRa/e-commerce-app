e8d0dca5a7163a1f93255cc9555042e145639b17
Nicolas <nicolasdirago@gmail.com>
2020-06-16 18:05:06 +0200

    updated shop data with different ids - implemented cart utils.js to handle cartitems in cart

--- src/components/redux/cart/cartActions.js
+++ src/components/redux/cart/cartActions.js
@@ -3,3 +3,8 @@ import { cartTypes } from './cartTypes'
 export const toggleCartHidden = () => ({
   type: cartTypes.TOGGLE_CART_HIDDEN
 })
+
+export const addItem = item => ({
+  type: cartTypes.ADD_ITEM,
+  payload: item
+})
--- src/components/ui/collection-item/CollectionItem.js
+++ src/components/ui/collection-item/CollectionItem.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import CustomButton from '../custom-button/CustomButton'
+import { addItem } from '../../redux/cart/cartActions'
 import './CollectionItem.scss'
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item
+
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,8 +14,15 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to Cart
+      </CustomButton>
     </div>
   )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
