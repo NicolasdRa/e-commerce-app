4ea196c697e224fe155f83ccf6929ea692659eb4
Nicolas <nicolasdirago@gmail.com>
2020-06-19 17:26:04 +0200

    updated shopSelector and components to  improve redux data flow

--- src/components/ui/collections-overview/CollectionsOverview.js
+++ src/components/ui/collections-overview/CollectionsOverview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
-import { selectCollections } from '../../redux/shop/shopSelector'
+import { selectCollectionsForPreview } from '../../redux/shop/shopSelector'
 
 import CollectionPreview from '../../ui/collection-preview/CollectionPreview'
 
@@ -19,7 +19,7 @@ const CollectionsOverview = ({ collections }) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
