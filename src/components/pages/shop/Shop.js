5d8a34cd0210adda1b417601ac81cc0d7eba30af
Nicolas <nicolasdirago@gmail.com>
2020-06-26 19:33:32 +0200

    changed "Observable pattern" into "Promise pattern" to get data from firestore at shop page

--- src/components/pages/shop/Shop.js
+++ src/components/pages/shop/Shop.js
@@ -30,7 +30,7 @@ class Shop extends Component {
     const { updateCollections } = this.props
     const collectionRef = db.collection('collections')
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
       this.setState({ loading: false })
